7c15b89a1222ab83d209670076cd9d6081e61f4c
Cody Roberts <codyjroberts@gmail.com>
2015-08-06 09:39:03 -0500

    Setup Guard

--- lib/chicrime.rb
+++ lib/chicrime.rb
@@ -1,5 +1,18 @@
 require "chicrime/version"
+require "soda/client"
 
 module Chicrime
-  # Your code goes here...
+  class Dataset
+    attr_accessor :client
+    attr_reader :dataset_id
+
+    def initialize token
+      @client = SODA::Client.new({:domain => 'data.cityofchicago.org', :app_token => token})
+      @dataset_id = 'ijzp-q8t2'
+    end
+
+    def id
+    end
+
+  end
 end
--- spec/chicrime_spec.rb
+++ spec/chicrime_spec.rb
@@ -1,7 +1,31 @@
 require 'spec_helper'
+require 'rspec/its'
 
 describe Chicrime do
   it 'has a version number' do
     expect(Chicrime::VERSION).not_to be nil
   end
+end
+
+describe Chicrime::Dataset do
+
+  before :all do
+    @dataset = Chicrime::Dataset.new('3iKjVXIj6i2Qj1oeEPr4kRqY2')
+  end
+
+  subject { @dataset }
+
+  it { should respond_to :id }
+
+  context "created with defaults" do
+    its(:client) { should be_an_instance_of SODA::Client }
+    its(:dataset_id) { should eq('ijzp-q8t2') }
+  end
+
+  describe '#id' do
+    it 'returns Hashie::Map object for crime with given id' do
+      pending        
+    end
+  end
+
 end
